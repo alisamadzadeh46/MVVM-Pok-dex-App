d0e90d3496bc8f09271e73c7a652430c229324f4
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-04-25 21:35:42 +0430

    first commit

---
+++ app/src/main/java/com/example/pokdex/PokdexApplication.kt
@@ -0,0 +1,15 @@
+package com.example.pokdex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+
+@HiltAndroidApp
+class PokdexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
